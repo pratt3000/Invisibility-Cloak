1a9aa5ace4366514d3087c52f92a67acbbb81b7a
pratt3000 <pratt3000@gmail.com>
2020-06-13 12:26:05 +0530

    editing

--- src/color_range_finder.py
+++ src/color_range_finder.py
@@ -1,7 +1,7 @@
 from cv2 import cv2 as cv
 import numpy as np
 
-# cloak color is color chosen
+# cloak color is color_chosen in RGB
 color_chosen = np.uint8([[[255, 0, 0]]])
-hsv_green = cv.cvtColor(color_chosen, cv.COLOR_BGR2HSV)
-print(hsv_green)
+hsv = cv.cvtColor(color_chosen, cv.COLOR_BGR2HSV)
+print(hsv)
